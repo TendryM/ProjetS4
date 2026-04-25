fc2230f58723d9d46fdac2338021292441f4f907
TendryM <manambitsoatendry@gmail.com>
2026-04-26 00:52:48 +0200

    Création des premiers composants React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 const title = "Bonjour les gens !"
-const style = {color: "white", backgroundColor: "red", padding: "10px", borderRadius: "5px"}
+const style = { color: "white", backgroundColor: "red", padding: "10px", borderRadius: "5px" }
 const ShowTitle = true
 const todos = [
   "Présenter react",
@@ -9,23 +9,56 @@ const todos = [
 
 function App() {
 
-const handleClick = (e) => {
-  console.log(e)
-  alert("Vous avez cliqué sur le titre !")
-}
+  const handleClick = (e) => {
+    console.log(e)
+    alert("Vous avez cliqué sur le titre !")
+  }
 
-  return <> 
-    {ShowTitle ? 
-    <h1 onClick={handleClick} id="title" className="titre" style={style}>{title}</h1> : 
-    <h1>Le titre est caché</h1>
-    }
+  return <>
+    <Title color="white" backgroundColor="red" />
+    <Title2 color="white" backgroundColor="grey">Mon composant Titre 2</Title2>
+    <Title2 color="white" backgroundColor="green" hidden>Mon composant Titre 2 Caché</Title2>
     <input type="text" />
-    <input type="checkbox"/>
-    <p>Coucou coucou coucou coucou</p>
+    <input type="checkbox" />
+    <p>Coucou coucou coucou coucou coucou coucou</p>
     <ul>
       {todos.map(todo => (<li key={todo}>{todo}</li>))}
     </ul>
-    </> 
+  </>
+}
+
+function Title (props) {
+  return <h1 style={
+    {color:props.color, 
+      backgroundColor:props.backgroundColor, 
+      borderRadius: "10px", 
+      padding: "10px",
+      fontFamily: "Arial, monospace",
+    }}>
+      Mon composant Titre
+      </h1>
+}
+
+function Title2 ({color, backgroundColor, children, hidden}) {
+
+  if (hidden) { {/* Détecte la présence/absence de la prop */}
+    return null
+  }
+  
+  const prop = {
+    id : "monid",
+    className: "maclasse"
+  }
+
+  return <h1 style={
+    {color: color, 
+      backgroundColor: backgroundColor, 
+      borderRadius: "10px", 
+      padding: "10px",
+      fontFamily: "Arial, monospace",
+    }} {...prop}>
+      {children} {/* Ce qui est mis entre les balises ouvrantes et fermantes*/}
+      </h1>
 }
 
 export default App
